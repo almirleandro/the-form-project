034f7f981f471e66408190462ce83b0607379b32
Almir Leandro <leandroalmm50@gmail.com>
2021-02-19 14:54:43 -0300

    Add movie info feature and a mock data file for development

--- src/Registered.tsx
+++ src/Registered.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+//import data from './mockData.js'; // For development, uncomment this and comment the fetch call at searchMovie()
 
 function Registered(props: {profile: JSX.Element | undefined, setCanEnter: React.Dispatch<React.SetStateAction<boolean>>}) {
 
@@ -23,7 +24,19 @@ function Registered(props: {profile: JSX.Element | undefined, setCanEnter: React
   }
 
   useEffect(() => {
-    const tempList = movieArray.map(movie => <img key={movie.id} src={'https://image.tmdb.org/t/p/w185' + movie.poster_path} alt='moviePoster'/>);
+    const tempList = movieArray.map(movie => {
+      return (
+        <div className='movieBlock' key={movie.id}>
+          <img key={movie.id} src={'https://image.tmdb.org/t/p/w185' + movie.poster_path} alt='moviePoster'/>
+          <div className='text'>
+            <p><b>Título: </b>{movie.title}</p>
+            <p><b>Título original: </b>{movie.original_title}</p>
+            <p><b>Data de lançamento: </b>{movie.release_date}</p>
+            <p><b>Nota no TMDB: </b>{movie.vote_average}</p>
+          </div>
+        </div>
+      )
+    });
     setMovieList(tempList);
   }, [movieArray])
 
@@ -31,7 +44,7 @@ function Registered(props: {profile: JSX.Element | undefined, setCanEnter: React
   return (
     <div className="Registered">
       {props.profile}
-      <h1>Great Movie Search</h1>
+      <h1>Great Movie Search</h1><br/>
       <input className='queryInput' placeholder='Search a movie' onChange={e => setQuery(e.target.value)} />
       <button onClick={() => searchMovie()}>Submit</button>
       <div className='movieGrid'>
